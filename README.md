d34766224f2c9760cfae7017621eae734d9b3650
Ardalan <ardalan.samimi@gmail.com>
2015-07-26 22:27:01 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,144 @@
+## Dropster
+Dropster abstracts and simplifies drag and drop and enables file uploads with AJAX.
+
+### Usage
+The Dropster plugin is depended on the jQuery plugin. Include it along with the plugin files, as shown below:
+```html
+  <script src="//code.jquery.com/jquery-2.1.4.min.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="/path/to/dropster-1.2.css" media="screen" charset="utf-8">
+  <script src="/path/to/dropster-1.2.js" charset="utf-8"></script>
+```
+You can also install it using npm:
+```bash
+$ npm install dropster
+```
+Use the plugin as follows (the url property is required):
+```js
+$(".dragAndDropElement").dropster({
+   "url": "/path/to/upload.php"
+});
+```
+Drag the file(s) over the drop zone, and just release. Easy schmeezy.
+
+![Screenshot](https://github.com/pkrll/JavaScript/blob/master/Dropster/screenshot.png)
+
+Let the upload commence!
+
+![Screenshot](https://github.com/pkrll/JavaScript/blob/master/Dropster/screenshot-1.png)
+![Screenshot](https://github.com/pkrll/JavaScript/blob/master/Dropster/screenshot-2.png)
+
+#### Options
+```js
+.dropster({
+        url               : "/path/to/server/upload/",
+        uploadLimit       : 2,
+        loaderImagePath   : "/path/to/image.png",
+        extensions        : ["jpg", "jpeg", "gif", "png"],
+        onDownload        : function(progressEvent)   { },
+        onUpload          : function(progressEvent)   { },
+        onReady           : function(responseText)    { },
+        onError           : function(errorMessage)    { }
+    });
+```
+##### Properties
+* `url`: The request URL path (**required**).
+* `uploadLimit`: Sets the limit on how many files should be uploaded at once.
+* `loaderImagePath`: Path to the loader image. Will be displayed if the default progress method is used.
+* `extensions`: List of allowed extension. Files with any other extensions will not be uploaded.
+* `onDownload`: Callback function, called during the XMLHttpRequests onprogress event, and is passed a progressEvent object.
+* `onUpload`: Callback function, called during the XMLHttpRequests upload.onprogress event, and is passed a progressEvent object.
+* `onReady`: Callback function, called when the request has finished and response is ready (readyState === 4). This method is passed the responseText property of the XMLHttpRequest object and will be in charge of parsing the server response.
+
+#### Customization
+Dropster's default progress monitoring can easily be overridden and tailored to fit your exact needs. Custom callbacks can also access the default progress methods, like so:
+```js
+   onReady: function(responseText) {
+      // .. do something here
+      this.defaultOnReady();
+   }
+```
+The custom callbacks also have access to these variables:
+* `totalSizeToLoad`: The total number of files that are to be uploaded. (**read-only**).
+* `totalSizeLoaded`: Number of files that as of accessing this variable has been uploaded. (**read-only**).
+
+#### Customization example
+Below follows an example, where the [ProgressBar plugin](https://github.com/pkrll/JavaScript/tree/master/Progressbar) is used instead of the default dialog window.
+```js
+    /**
+     * Add the Dropster plugin to the element with
+     * id targetArea, with customized settings.
+     */
+    $("#targetArea").dropster({
+       url: "/upload/image",
+       uploadLimit: 5,
+       extensions: ["jpg", "jpeg", "png", "gif"],
+       onUpload: $.fn.onUpload,
+       onDownload: $.fn.onDownload,
+       onReady: function (response) {
+         // Remove the progressbar
+         // and parse the response.
+         this.monitor.remove();
+         var parsedResponse = jQuery.parseJSON(response);
+         // Print out the response
+         console.log(parsedResponse);
+       }
+    });
+    /**
+     * Override Dropster's default onUpload function.
+     *
+     * @param   progressEvent
+     */
+    $.fn.onUpload = function (event) {
+        // Access the plugin's public interface using
+        // the "this" keyword, enabling a place to store
+        // the progressbar element, instead of declaring
+        // it as a global variable.
+        var self = this;
+        // Create the progress bar object, and
+        // connect it to the Dropster plugin, if
+        // it already does not exist. But keep it
+        // inside a conditional statement, so that
+        // we do not create loads of progress bars.
+        if ($("#progress-bar-container").length < 1) {
+            var element = $("<div>").attr({
+                "id": "progress-bar-container"
+            }).appendTo("body");
+            self.monitor = new ProgressBar ({ parentElement: element });
+            self.monitor.createBar();
+        }
+        // Calculate upload progress
+        var completed = 0;
+        if (event.lengthComputable) {
+            // The uploading process is only part
+            // one of the whole process in this
+            // example, that also includes server
+            // side computation. Therefore, divide
+            // this status by two, and handle the
+            // rest of it through the onDownload function.
+            completed = Math.round((event.loaded / event.total * 1000) / 10 / 2);
+            self.monitor.setProgress(completed);
+        }
+    }
+    /**
+     * Override Dropster's default function onDownload function,
+     * for when the server sends information back.
+     *
+     * @param   progressEvent
+     */
+    $.fn.onDownload = function (event) {
+        // onDownload will monitor the response from the server,
+        // for example if the server is streaming information
+        // back in real time. In this example, we will assume
+        // the server is sending back information about the
+        // images, one by one as they are being processed.
+        var totalSizeToLoad = this.totalSizeToLoad;
+        var currentProgress = this.monitor.getProgress();
+        // Calculate the percental of each item uploaded and
+        // add it to the current progress of the progress bar.
+        var completed = (Math.round((1 / totalSizeToLoad * 1000) / 10 / 2) + currentProgress);
+        // Set the new status of the progress bar.
+        this.monitor.setProgress(completed);
+    }
+```
+### Author
+* Dropster was created by Ardalan Samimi.
---
+++ src/dropster-1.2.js
@@ -0,0 +1,377 @@
+/**
+ * Javascript plugin
+ * Dropster 1.2
+ *
+ * Adds drag and drop functionality to an HTML element,
+ * and uploading files to the server.
+ *
+ * @version 1.2
+ * @author Ardalan Samimi
+ */
+(function($) {
+    /**
+     * Dropster constructor.
+     *
+     * @param   string  The element using the plugin
+     * @param   object  A collection of settings.
+     */
+    var Dropster = function (element, options) {
+        this.element    = $(element);
+        this.settings   = $.extend({
+            url               : false,
+            uploadLimit       : 0,
+            loaderImagePath   : "/node_modules/dropster/lib/loading-128.png",
+            extensions        : ["jpg", "jpeg", "gif", "png"],
+            monitor           : false,
+            onDownload        : function(progressEvent)   { },
+            onUpload          : function(progressEvent)   { },
+            onReady           : function(responseText)    { },
+            onError           : function(errorMessage)    { }
+        }, options || {});
+        // Set the public interface property.
+        this.setPublicInterface();
+        // Bind the drag events
+        this.bindDragEvents();
+    };
+
+    Dropster.prototype = {
+        /**
+         * Default monitoring function for downloading and
+         * getting response from the server. Will be called
+         * if not overriden.
+         *
+         * @param   progressEvent   The progressEvent object from XMLHttpRequest onprogress
+         */
+        onDownload: function (event) {
+            // Calculate the progress and show it.
+            var completed = (Math.round((++this.publicInterface.totalSizeLoaded / this.publicInterface.totalSizeToLoad * 1000) / 10));
+            this.onProgress("Uploading files... " + completed + "%");
+        },
+        /**
+         * Default monitoring function for uploading to the
+         * server, will be called if not overriden.
+         *
+         * @param   progressEvent   The progressEvent object from XMLHttpRequest upload.onprogress
+         */
+        onUpload: function (event) {
+            // Calculate the progress and show it.
+            var completed   = 0;
+            if (event.lengthComputable)
+                completed = Math.round((event.loaded / event.total * 1000) / 10);
+            this.onProgress("Uploading files... " + completed + "%");
+        },
+        /**
+         * The default ready function called when the upload
+         * process is finished, will be called if not overriden.
+         *
+         * @param   mixed   Response property of an XMLHttpRequest object
+         */
+        onReady: function (responseText) {
+            if ($("#dropster-window").length < 1)
+                this.createWindow(false);
+            var divHeader   = $(".dropster-window-header");
+            var divBody     = $(".dropster-window-body");
+            // Set the new status
+            divHeader.html("Upload finish!");
+            divBody.children().remove();
+            divBody.html("The images have been successfully uploaded.");
+        },
+        /**
+         * Called when encountering error. Can be ovverriden.
+         *
+         * @param   string  The error message
+         */
+        onError: function (message) {
+            if ($("#dropster-window").length < 1)
+                this.createWindow(false);
+            var divHeader   = $(".dropster-window-header");
+            var divBody     = $(".dropster-window-body");
+            // Set the new status
+            divHeader.html("Upload error");
+            divBody.children().remove();
+            divBody.html("An error has occurred: " + message);
+        },
+        /**
+         * Creates a public interface property comprised of
+         * the public methods and variables for user access.
+         *
+         */
+        setPublicInterface: function () {
+            // The regular expression pattern for an empty function
+            var emptyFunction = new RegExp(/(\{\s\})|(\{\})/);
+            var publicMethods = {};
+            // Loop through the settings variable and
+            // check for all the methods defined there.
+            for (method in this.settings) {
+                if ($.isFunction(this.settings[method])) {
+                    var string = this.settings[method].toString();
+                    // Empty methods (that is, methods not overriden) should be
+                    // replaced by the default method in the public interface,
+                    // otherwise, also add a default* method too.
+                    if (emptyFunction.test(string)) {
+                        if ($.isFunction(this[method]))
+                            publicMethods[method] = this[method].bind(this);
+                    } else {
+                        // Create the defaultMethodName
+                        var defaultMethodName = this.createDefaultMethodName(method);
+                        publicMethods[method] = this.settings[method];
+                        publicMethods[defaultMethodName] = this[method].bind(this);
+                    }
+                }
+            }
+            // Set the public interface property, that will hold
+            // both the public methods and the public properties.
+            this.publicInterface                  = publicMethods;
+            this.publicInterface.monitor          = this.settings.monitor;
+            this.publicInterface.totalSizeToLoad  = 0;
+            this.publicInterface.totalSizeLoaded  = 0;
+        },
+        /**
+         * Binds the elements and DOM drag events.
+         * Called upon initialization.
+         *
+         */
+        bindDragEvents: function() {
+            var self = this;
+            var elem = this.element;;
+            // Bind the elements drag events
+            elem.on("dragenter", function (event) { self.dragEnter(event, elem); });
+            elem.on("dragover", function (event) { self.dragOver(event); });
+            elem.on("drop", function (event) { self.drop(event, elem); });
+            elem.on("dragleave", function (event) { self.dragLeave (event); });
+            // Bind the drag events to the DOM, to
+            // make sure the highlighting works.
+            $(document).on("dragenter", function (event) { self.dragEnter(event); });
+            $(document).on("drop", function (event) { self.drop(event); });
+            $(document).on("dragover", function (event) { self.dragOver(event, elem); });
+        },
+        /**
+         * Watches for when a dragged object enters
+         * a valid drop zone.
+         *
+         * @param   eventObject
+         * @param   Element
+         */
+        dragEnter: function (event, elem) {
+            $('.dropster-highlight').removeClass('dropster-highlight');
+            var elem = elem || false;
+            if (elem !== false)
+                elem.addClass("dropster-highlight");
+            this.onDrop(event);
+        },
+        /**
+         * Watches for when an object is being
+         * dragged over the valid drop zone. If
+         * an element is provided, it will be
+         * stripped of the highlighting. This
+         * is for when the dragged object leaves
+         * the drop target.
+         *
+         * @param eventObject
+         * @param Element
+         */
+         dragOver: function (event, elem) {
+             var elem = elem || false;
+             if (elem !== false)
+                elem.removeClass("dropster-highlight");
+            this.onDrop(event);
+         },
+        /**
+         * Watches for when an object leaves
+         * the drop area. If an element is
+         * provided, it will be stripped of
+         * the highlighting.
+         *
+         * @param eventObject
+         * @param Element
+         */
+         dragLeave: function (event, elem) {
+             var elem = elem || false;
+             if (elem !== false)
+                elem.removeClass("dropster-highlight");
+            this.dragOver(event);
+        },
+        /**
+         * Watches for when a dragged object
+         * is dropped on a valid drop zone.
+         *
+         * @param eventObject
+         * @param Element
+         */
+         drop: function (event, elem) {
+             var elem   = elem || false;
+             var upload = false;
+             if (elem !== false) {
+                 elem.removeClass("dropster-highlight");
+                 upload = true;
+             }
+             this.onDrop(event, upload);
+        },
+        /**
+         * Determines if the drag/drop was valid
+         * and creates a formData object to send
+         * to the function sendDrop.
+         *
+         * @param eventObject   To stop default actions
+         * @param        bool   Is upload, yes?
+         */
+         onDrop: function (event, upload) {
+             var upload = upload || false;
+             event.stopPropagation();
+             event.preventDefault();
+             // Check if an object was dropped on the
+             // designated target area or not.
+             if (upload === true) {
+                 // Sort through the files that were
+                 // dropped and add it to the formdata.
+                 var files  = event.originalEvent.dataTransfer.files;
+                 var fData  = new FormData();
+                 var error  = false;
+                 var self   = this;
+                 $.each(files, function(x, file) {
+                     // If the user has tried uploading files with
+                     //  anything other than the allowed extensions
+                     // then abort the whole operation.
+                     if (self.checkExtension(file.name) === false) {
+                         var onError = self.getSetting("onError");
+                         if (onError === false) {
+                            self.onError("Could not upload file " + file.name + ". File extension not allowed.");
+                        } else {
+                            onError("Could not upload file " + file.name + ". File extension not allowed.");
+                        }
+                         error = true;
+                         return false;
+                     } else {
+                         // Append files to formdata
+                         fData.append('file'+x, file);
+                     }
+                     // Stop if the limit has been reached
+                     if (self.settings.uploadLimit !== 0 &&
+                         self.settings.uploadLimit == (x+1))
+                         return false;
+                 });
+                 if (error)
+                    return false;
+                 // Send the dropped files
+                 this.sendDrop(fData, files.length);
+             }
+         },
+         /**
+          * Send the files to the server.
+          *
+          * @param  formData    The data to be sent
+          * @param       int    Size of the package
+          */
+         sendDrop: function (dataPackage, packageSize) {
+             // The data package and size is crucial
+             var dataPackage = dataPackage || false,
+                 packageSize = packageSize || false;
+             if (dataPackage === false || packageSize === false)
+                return false;
+            // Declare self as the object, for scope
+            // issues. Also, declare the XMLHttpRequest.
+            var self = this;
+            var xhr = new XMLHttpRequest();
+            self.publicInterface.totalSizeToLoad = packageSize;
+            // Set the upload monitoring, either the
+            // default or user overriden function.
+            xhr.upload.onprogress = function (event) {
+                self.publicInterface.onUpload(event);
+            }
+            // Set the download monitoring, either the
+            // default or user overriden function.
+            xhr.onprogress = function (event) {
+                self.publicInterface.onDownload(event);
+            }
+            // When the image upload is done, run either
+            // the user defined or the default function.
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    self.publicInterface.onReady(xhr.responseText);
+                }
+            }
+            // Make the request, only if an URL is set.
+            if (self.settings.url !== false) {
+                xhr.open('POST', self.settings.url);
+        		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        		xhr.send(dataPackage);
+            } else {
+                self.publicInterface.onError("No upload path set");
+            }
+        },
+        /**
+         * Check the extension of the file(s) to be uploaded,
+         * comparing to the array of file extensions set in
+         * the array extensions.
+         *
+         * @param   string  Name of file
+         * @returns   bool
+         */
+        checkExtension: function (fileName) {
+            extension = fileName.split('.').pop().toLowerCase();
+            if ($.inArray(extension, this.settings.extensions) != -1)
+                return true;
+            return false;
+        },
+        /**
+         * Prepends method name with string
+         * "default", using camelCase.
+         *
+         * @param   string  Name to manipulate
+         * @returns string
+         */
+        createDefaultMethodName: function(name) {
+            if (/[A-Z]/.test(name.charAt(0)))
+                return "default" + name;
+            var firstLetter = name.charAt(0);
+            return "default" + firstLetter.toUpperCase() + name.substring(1);
+        },
+        /**
+         * Creates a dialog window, for the default progress
+         * monitoring event.
+         *
+         * @param   bool    If a spinner should be shown.
+         */
+        createWindow: function (withLoaderImage) {
+            var withLoaderImage = withLoaderImage || false;
+            var div = $("<div>");
+            // Create the dialog window
+            var divWindow   = div.clone().attr({"id": "dropster-window"});
+            var divHeader   = div.clone().attr({"class": "dropster-window-header"}).appendTo(divWindow);
+            var divBody     = div.clone().attr({"class": "dropster-window-body"}).appendTo(divWindow);
+            var divFooter   = div.clone().attr({"class": "dropster-window-footer"}).appendTo(divWindow);
+            var divButton   = div.clone().attr({"class": "dropster-window-button"}).html("OK").appendTo(divFooter);
+            // Bind the button
+            divButton.on("click", function() {
+                $("#dropster-window").remove();
+            });
+            // Adds a spinner image
+            if (withLoaderImage)
+                var imgElement  = $("<img>").attr({"src": this.settings.loaderImagePath}).appendTo(divBody);
+            // Show the window
+            divWindow.appendTo("body");
+        },
+        /**
+         * Outputs the progress.
+         *
+         * @param   string  The output string
+         */
+        onProgress: function (progress) {
+            if ($("#dropster-window").length < 1)
+                this.createWindow(true);
+            // Find the header and show the progress
+            $(".dropster-window-header").html(progress);
+        }
+    };
+
+    $.fn.dropster = function (options) {
+        return this.each(function() {
+            // Save the instance to make sure the plugin does
+            // not get called multiple times on same element.
+            var instance = $(this).data("plugin_dropster");
+            if (!instance)
+                $(this).data("plugin_dropster", new Dropster(this, options));
+        });
+    };
+
+})(jQuery);
